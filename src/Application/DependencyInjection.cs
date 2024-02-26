b48012ca3e894131eec4dc773b05ee6777b62cec
Aldis Adomavičius <aldisadom@gmail.com>
2024-02-26 12:33:50 +0200

    Initial base client

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -8,7 +8,6 @@ public static class DependencyInjection
 {
     public static void AddApplication(this IServiceCollection services)
     {
-        services.AddScoped<IClientService, ClientService>();
         services.AddScoped<IItemService, ItemService>();
     }
 }
